b448528eb59e00b8f05b8f45dca8a3398a5a11f1
chiritagabriela <chirita.gabi@yahoo.com>
2020-12-16 16:25:15 +0200

    Imbunatatire securitate aplicatie :shield:
    Signed-off-by: chiritagabriela <chirita.gabi@yahoo.com>

---
+++ MTADroneService_server/DroneService/src/main/java/MTADroneService/DroneService/authentification/config/security/MTADroneServiceAuthProvider.java
@@ -0,0 +1,72 @@
+package MTADroneService.DroneService.authentification.config.security;
+
+import MTADroneService.DroneService.authentification.daos.UserDAO;
+import MTADroneService.DroneService.authentification.exceptions.InvalidTokenException;
+import MTADroneService.DroneService.authentification.exceptions.TokenAuthException;
+import MTADroneService.DroneService.authentification.models.UserModel;
+import MTADroneService.DroneService.authentification.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+import static sun.util.locale.LocaleUtils.isEmpty;
+
+@Component
+public class MTADroneServiceAuthProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    @Autowired
+    UserDAO userDAO;
+
+    @Autowired
+    TokenService tokenService;
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        final String token = (String) authentication.getCredentials();
+
+        if (isEmpty(token)) {
+            return new User(username, "", AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
+        }
+
+        Optional<UserModel> userModelOptional = userDAO.findByAuthToken(token);
+
+        if (userModelOptional.isPresent()) {
+            final UserModel userModel = userModelOptional.get();
+
+            try {
+                tokenService.validateToken(token);
+            } catch (InvalidTokenException e) {
+                userModel.setAuthToken(null);
+                userDAO.save(userModel);
+                return null;
+            }
+
+            return new User(username, "",
+                    AuthorityUtils.createAuthorityList(
+                            userModel.getRoles().stream()
+                                    .map(roleName -> "ROLE_" + roleName)
+                                    .toArray(String[]::new)
+                    )
+            );
+        }
+
+        try {
+            throw new TokenAuthException("User not found for token :" + token);
+        } catch (TokenAuthException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ MTADroneService_server/DroneService/src/main/java/MTADroneService/DroneService/authentification/exceptions/TokenAuthException.java
@@ -0,0 +1,8 @@
+package MTADroneService.DroneService.authentification.exceptions;
+
+import javax.naming.AuthenticationException;
+
+public class TokenAuthException extends AuthenticationException {
+    public TokenAuthException(String s) {
+    }
+}
