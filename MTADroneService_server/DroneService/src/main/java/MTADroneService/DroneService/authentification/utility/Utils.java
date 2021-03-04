10e93ba15c0f87268c92884f4755366a6ebea5b6
chiritagabriela <chirita.gabi@yahoo.com>
2021-03-04 22:42:01 +0200

    Primire imagine de la server :cloud:

--- MTADroneService_server/DroneService/src/main/java/MTADroneService/DroneService/authentification/utility/Utils.java
+++ MTADroneService_server/DroneService/src/main/java/MTADroneService/DroneService/authentification/utility/Utils.java
@@ -1,5 +1,16 @@
 package MTADroneService.DroneService.authentification.utility;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -7,6 +18,7 @@ public class Utils {
 
     public static String SERVER_IP = "127.0.0.1";
     public static int PORT = 9999;
+    public static int imageNumber = 0;
 
     public enum MissionTypes {
         SAR,
@@ -30,7 +42,28 @@ public class Utils {
     }
     public static Date getCurrentDate(){
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = new Date(System.currentTimeMillis());
-        return date;
+        return new Date(System.currentTimeMillis());
+    }
+
+    public static void saveImage(byte[] byteImage) throws IOException {
+        ImageIcon imageIcon = new ImageIcon(byteImage);
+        Image image = imageIcon.getImage();
+        RenderedImage rendered = null;
+        if (image instanceof RenderedImage) {
+            rendered = (RenderedImage)image;
+        }
+        else {
+            BufferedImage buffered = new BufferedImage(
+                    imageIcon.getIconWidth(),
+                    imageIcon.getIconHeight(),
+                    BufferedImage.TYPE_INT_RGB
+            );
+            Graphics2D g = buffered.createGraphics();
+            g.drawImage(image, 0, 0, null);
+            g.dispose();
+            rendered = buffered;
+        }
+        ImageIO.write(rendered, "JPG", new File(imageNumber+".jpg"));
+        imageNumber = imageNumber + 1;
     }
 }
--- MTADroneService_server/DroneService/src/main/java/MTADroneService/DroneService/authentification/utility/implementation/ClientImpl.java
+++ MTADroneService_server/DroneService/src/main/java/MTADroneService/DroneService/authentification/utility/implementation/ClientImpl.java
@@ -1,23 +1,28 @@
 package MTADroneService.DroneService.authentification.utility.implementation;
 
 import MTADroneService.DroneService.authentification.utility.Client;
+import MTADroneService.DroneService.authentification.utility.Utils;
 import com.google.common.primitives.Ints;
+import com.sun.mail.iap.ByteArray;
+import org.springframework.data.mongodb.core.aggregation.ConvertOperators;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
 import java.io.*;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class ClientImpl implements Client {
     private Socket clientSocket;
     private OutputStream out;
-    private BufferedReader in;
+    private InputStream in;
 
     @Override
     public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
         out = clientSocket.getOutputStream();
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        in = clientSocket.getInputStream();
     }
 
     @Override
@@ -28,27 +33,25 @@ public class ClientImpl implements Client {
     }
 
     @Override
-    public void sendImageToServer(MultipartFile file) throws IOException {
-        byte[] bytesImage = file.getBytes();
-        int nrBytes = bytesImage.length;
-        int currentLength = 0;
-        int difference = -1;
-
-        while (difference != 0) {
-            difference = nrBytes - currentLength;
-            if (difference < 8192 && difference != 0) {
-                currentLength = currentLength + difference;
-                out.write(getNextByteBlock(bytesImage,currentLength-difference,currentLength));
-            }
-            else{
-                if(difference != 0) {
-                    currentLength = currentLength + 8192;
-                    out.write(getNextByteBlock(bytesImage, currentLength - 8192, currentLength));
-                }
+    public void receiveImageFromServer() throws IOException {
+        byte[] nrBytes = new byte[4];
+        int okTotalLength = in.read(nrBytes);
+        int imageLen = bytesToInt(nrBytes,findPadding(nrBytes));
+        byte[] byteImage = new byte[imageLen];
+        int currentImgLen = 0;
+        int getOut = 0;
+        while(getOut == 0) {
+            int okChunksLength = in.read(nrBytes);
+            int chunkLen = bytesToInt(nrBytes,findPadding(nrBytes));
+            byte[] receivedImage = new byte[chunkLen];
+            int okPhoto = in.read(receivedImage,0,chunkLen);
+            concatenateImageChunks(byteImage, receivedImage, currentImgLen);
+            currentImgLen = currentImgLen + chunkLen;
+            if(currentImgLen == imageLen){
+                getOut = 1;
             }
         }
-        byte[] stop = ("s").getBytes();
-        out.write(stop);
+        Utils.saveImage(byteImage);
     }
     private byte[] getNextByteBlock(byte[] totalBytes, int start, int end){
         byte[] currentBlock = new byte[8192];
@@ -58,5 +61,29 @@ public class ClientImpl implements Client {
             iterator++;
         }
         return currentBlock;
+    }
+    private void concatenateImageChunks(byte[] imageArray, byte[] receivedImage, int byteImgLen) {
+        int cnt = 0;
+        for (int i=byteImgLen; i<byteImgLen + receivedImage.length;i++){
+            imageArray[i] = receivedImage[cnt];
+            cnt = cnt + 1;
+        }
+    }
+
+    private int bytesToInt(byte[] array, int nrBytes){
+        int nr = 0;
+        for(int i=0;i<nrBytes;i++){
+            nr = nr + (int)Math.pow(10,nrBytes-1-i)*((int)array[i]-48);
+        }
+        return nr;
+    }
+    private int findPadding(byte[] array) {
+        int nr = 0;
+        for (byte b : array) {
+            if (b == 33) {
+                nr++;
+            }
+        }
+        return array.length-nr;
     }
 }
